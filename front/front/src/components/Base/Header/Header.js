264cfdd52aebdd5f2aab39611a51b53a5a9209f8
bluekmky <bluekmky@naver.com>
2019-12-03 00:32:07 +0900

    header style commit

--- front/front/src/components/Base/Header/Header.js
+++ front/front/src/components/Base/Header/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import oc from 'open-color';
-import Button from '@material-ui/core/Button';
 import { shadow, media } from '../../../lib/styleUtil';
 
 // 상단 고정, 그림자
@@ -16,7 +15,7 @@ const Positioner = styled.div`
 
 // 흰 배경, 내용 중간 정렬
 const WhiteBackground = styled.div`
-    background: white;
+    background: #ffffff;
     display: flex;
     justify-content: center;
     height: auto;
@@ -43,10 +42,10 @@ const HeaderContents = styled.div`
 
 // 로고
 const Logo = styled.div`
-    font-size: 1.4rem;
+    font-size: 1.6rem;
     letter-spacing: 2px;
-    color: ${oc.teal[7]};
-    font-family: 'Rajdhani';
+    color: ${oc.gray[7]};
+    font-family: 'Alatsi', sans-serif;
 `;
 
 // 중간 여백
@@ -57,7 +56,7 @@ const Spacer = styled.div`
 // 하단 그래디언트 테두리
 const GradientBorder = styled.div`
     height: 3px;
-    background: linear-gradient(to right, ${oc.teal[6]}, ${oc.cyan[5]});
+    background: linear-gradient(to right, ${oc.gray[9]}, ${oc.gray[7]}, ${oc.gray[8]});
 `;
 
 const Header = ({children}) => {
@@ -65,11 +64,7 @@ const Header = ({children}) => {
         <Positioner>
             <WhiteBackground>
                 <HeaderContents>
-                    <Logo>HEURM</Logo>
-                    <Spacer/>
-                        <Button variant="contained" color="primary">
-                            Hello World
-                        </Button>
+                    <Logo>1701</Logo>
                     <Spacer/>
                     {children}
                 </HeaderContents>
