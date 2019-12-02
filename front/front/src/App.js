f8f2dd246890a951d76cfb1864a48874f55471fc
bluekmky <bluekmky@naver.com>
2019-12-02 23:41:39 +0900

    header commit

--- front/front/src/App.js
+++ front/front/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { Home, Auth } from './pages';
+import HeaderContainer from './components/Base/HeaderContainer'
+
 
 function App() {
   return (
     <div>
+      <HeaderContainer/>
       <Route exact path="/" component={Home}/>
       <Route path="/auth" component={Auth}/>
     </div>
